0dcefb1f71aa9d6f78659f8207ae97b268815d68
ButonJumadi <43023966+ButonJumadi@users.noreply.github.com>
2019-01-18 04:12:31 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = "172.24.0.4";
+$databaseName = "tabel";
+$databaseUsername = "root";
+$databasePassword = "test";
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
---
+++ tabel.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `age` int(3) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
